ed9cc4de5716f3ce02a70f9a101d67adf6e90617
yuchanglin <yuchanglinlin@163.com>
2024-03-12 17:04:38 +0800

    init commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const stringjudgement = {
+    isNull (string) {
+        return string === null
+    },
+    isEmpty (string) {
+        return string === ''
+    },
+    isUndefined (string) {
+        return string === undefined
+    },
+    isNullOrEmptyOrUndefined (string) {
+        return string === null || string === '' || string === undefined
+    }
+}
+
+module.exports = stringjudgement
